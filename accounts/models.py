227ec9349aa576b64062c8edfcf51a5d257a2382
cheachea <heekyung4105@gmail.com>
2025-02-19 22:23:34 +0900

    refactor: add  accounts/models.py , update accounts/urls.py

--- accounts/models.py
+++ accounts/models.py
@@ -5,4 +5,5 @@ from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
-    pass
+    def __str__(self):
+        return self.username
--- accounts/urls.py
+++ accounts/urls.py
@@ -7,10 +7,10 @@ urlpatterns = [
     # 토큰 갱신 - 프론트에서 필요
     path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     # 회원가입/탈퇴/로그인/로그아웃
-    path("user/signup/", SignupAPIView.as_view(), name="signup"),
-    path("user/delete/", DeleteAPIView.as_view(), name="delete"),
+    path("signup/", SignupAPIView.as_view(), name="signup"),
+    path("delete/", DeleteAPIView.as_view(), name="delete"),
     path(
-        "auth/login/", TokenObtainPairView.as_view(), name="login"
+        "login/", TokenObtainPairView.as_view(), name="login"
     ),  # 로그인 : JWT 기본제공
-    path("auth/logout/", LogoutAPIView.as_view(), name="logout"),
+    path("logout/", LogoutAPIView.as_view(), name="logout"),
 ]

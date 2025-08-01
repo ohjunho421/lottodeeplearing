236bb53aa82a6fb991991d3eae8a6db81e4abb69
Rorarion <wnsghcoswp@gmail.com>
2025-08-01 10:35:05 +0900

    Add csrf_exempt decorator to login URL to completely bypass CSRF validation - emergency fix for persistent Railway authentication issues

--- lottobot/urls.py
+++ lottobot/urls.py
@@ -6,6 +6,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import views as auth_views
+from django.views.decorators.csrf import csrf_exempt
 from django.views.generic import RedirectView
 from . import views
 
@@ -20,7 +21,7 @@ urlpatterns = [
     # Login redirect
     path('home/', RedirectView.as_view(url='/login/', permanent=False), name='home'),
     # 로그인/로그아웃/회원가입 URL
-    path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
+    path('login/', csrf_exempt(auth_views.LoginView.as_view(template_name='registration/login.html')), name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
     path('register/', views.register_view, name='register'),  # 회원가입 URL 추가
     
